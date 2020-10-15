45191b1c075897e85a8f2e6d0c63800d8da68956
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-15 15:57:27 -0300

    routes folder added

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3000;
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-  next()
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In product middleware");
-  res.send(
-    '<html><link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> product middleware</html>'
-  );
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("In root middleware");
-  res.send(
-    "<html><link rel='shortcut icon' href='data:image/x-icon;,' type='image/x-icon'>slash middleware</html>"
-  );
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server up and running at http://localhost:${port}`);
+});
