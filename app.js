4213296f17d7f957307944e98d03dc234f0317c7
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-15 22:45:50 -0300

    serve html files and basic css added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
+const path = require('path');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
+
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server up and running at http://localhost:${port}`);
