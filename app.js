c7021e071eab851070fa2a5436e758114ee598c9
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-17 16:05:44 -0300

    pug layout added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle : 'Page not found!'});
 });
 
 app.listen(port, () => {
