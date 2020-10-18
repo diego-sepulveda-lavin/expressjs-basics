b09e600d73e9335a6f3293f1b42725e07c06d24f
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-18 18:00:45 -0300

    handlebars template engine added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs' }));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -21,7 +24,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-  res.status(404).render('404', {pageTitle : 'Page not found!'});
+  res.status(404).render('404', { pageTitle: 'Page not found!' });
 });
 
 app.listen(port, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,21 @@
-const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path');
-
+const express = require('express');
 const router = express.Router();
+
+const rootDir = require('../util/path');
 
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
