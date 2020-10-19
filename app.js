40b57c30e48c74e376415b143c55f656e9e02ba7
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-19 14:11:33 -0300

    ejs template engine added

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs' }));
-app.set('view engine', 'hbs');
+//app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs' }));
+//app.set('view engine', 'hbs');
+
+//app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -24,7 +28,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: 'Page not found!' });
+  res.status(404).render('404', { pageTitle: 'Page not found!', path: '' });
 });
 
 app.listen(port, () => {
