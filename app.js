c5cc3a8b61c49247167404284816fec39ccca272
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-20 18:23:00 -0300

    MVC model added

--- app.js
+++ app.js
@@ -14,22 +14,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
+const errorController = require('./controllers/error');
 
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-  res.status(404).render('404', { pageTitle: 'Page not found!', path: '' });
-});
+app.use(errorController.get404);
 
 app.listen(port, () => {
   console.log(`Server up and running at http://localhost:${port}`);
