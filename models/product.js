f8ba63f98776f78f12329ec3efd3a013e682c5c3
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-24 16:41:55 -0300

    edit product logic added

--- models/product.js
+++ models/product.js
@@ -14,7 +14,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -22,14 +23,21 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        if (err) {
+      if (this.id) {
+        const existingProductIndex = products.findIndex((prod) => prod.id === this.id);
+        const updatedProduct = [...products];
+        updatedProduct[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
           console.log(err);
-        }
-      });
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,8 @@ const router = express.Router();
 // routes
 router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 router.get('/products', adminController.getProducts);
 
 module.exports = router;
