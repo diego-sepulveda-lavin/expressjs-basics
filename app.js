b8f634c49176b49cb662971b371d6dd503f48f19
Diego Sepulveda <diegosl@hotmail.cl>
2020-10-31 15:16:04 -0300

    relationships created, cartItem model created

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const sequelize = require('./util/database');
 //import models
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 //import other tools
 const rootDir = require('./util/path');
@@ -36,6 +38,15 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -43,12 +54,30 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true }) // only use on development, will cause to overwrite the tables
+  //.sync({ force: true }) // only use on development, will cause to overwrite the tables
+  .sync()
   .then((result) => {
+    return User.findByPk(1);
     //console.log(result);
     // app listening
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Diego', email: 'test@test.com' });
+    }
+    return Promise.resolve(user);
+  })
+  .then((user) => {
+    //console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(port, () => {
       console.log(`Server up and running at http://localhost:${port}`);
     });
